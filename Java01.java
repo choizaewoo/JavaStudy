f2b2e3d0ffcf72d377a04388c6daac621a1f7a89
1994cjw@gmail.com <1994cjw@gmail.com>
2022-06-14 01:22:25 +0900

    객체지향프로그래밍 - 인스턴스 생성과 사용

---
+++ Java01.java
@@ -0,0 +1,28 @@
+package Study;
+class Tv {
+	//Tv의 속
+	String color;
+	boolean power;
+	int channel;
+	
+	//Tv의 기능(메소드)
+	void power() {
+		power = !power;
+	}
+	void channelUp() {
+		++channel;
+	}
+	void channelDown() {
+		--channel;
+	}	
+}
+
+class TvTest {
+	public static void main(String[] args) {
+		Tv t;
+		t = new Tv();
+		t.channel = 7;
+		t.channelDown();
+		System.out.println("현재채널은 " +t.channel + " 입니다.");
+	}
+}
